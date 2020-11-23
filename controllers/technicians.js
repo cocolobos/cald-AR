7e9dfc845c60cf2ec5704e0039701212340f6c93
Nicolas Lobos <nicolasmlobos@gmail.com>
2020-11-23 16:40:14 -0300

    CAL-2-technicians functions-getByAttribute

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -23,4 +23,14 @@ app.delete('/technicians/:id', (req,res) => {
         res.status(400).json({msg:"Technician not found"})
     }
 })
+//GET TECHNICIANS BY ATTRIBUTE
+app.get('/technicians/:attribute/:value', (req, res) => {
+    const parseValue = isNaN(parseInt(req.params.value))? req.params.value : parseInt(req.params.value);
+    const found = technicians.some(tech => tech[req.params.attribute] === parseValue);
+    if(found){
+        res.json(technicians.filter(tech => tech[req.params.attribute] === parseValue));
+    }else{
+        res.status(400).json({msg:"Technician not found"})
+    }
+})
 module.exports = app;
