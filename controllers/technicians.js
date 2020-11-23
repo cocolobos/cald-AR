f74048304302e2cb40ebfa390c5e2e8ca95dd2db
Nicolas Lobos <nicolasmlobos@gmail.com>
2020-11-23 13:25:21 -0300

    CAL-2-technicians functions deleteById - getById

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -1,24 +1,24 @@
 const express = require ('express');
 const app = express();
-const tech = require ('../data/technicians-data.json');
+const technicians = require ('../data/technicians-data.json');
 
 //GET ALL TECHNICIANS
-app.get('/technicians', (req, res) => res.json(tech))
+app.get('/technicians', (req, res) => res.json(technicians))
 
 //GET TECHNICIANS BY ID
 app.get('/technicians/:id', (req, res) => {
-    const found = tech.some(tech => tech.id === parseInt(req.params.id));
+    const found = technicians.some(tech => tech.id === parseInt(req.params.id));
     if(found){
-        res.json(tech.filter(tech => tech.id === parseInt(req.params.id)));
+        res.json(technicians.filter(tech => tech.id === parseInt(req.params.id)));
     }else{
         res.status(400).json({msg: "No technician was found"})
     }
 })
 //DELETE TECHNICIANS
-app.delete('/Technicians/:id', (req,res) => {
-    const found = tech.some(tech => tech.id === parseInt(req.params.id));
+app.delete('/technicians/:id', (req,res) => {
+    const found = technicians.some(tech => tech.id === parseInt(req.params.id));
     if(found){
-        res.json({msg:"Technician deleted"})
+        res.json(technicians.filter(tech => tech.id !== parseInt(req.params.id)));
     }else{
         res.status(400).json({msg:"Technician not found"})
     }
