6bda63619727c5fac40d472906875731ad73ccaf
Nicolas Lobos <nicolasmlobos@gmail.com>
2020-11-24 19:45:26 -0300

    CAL-2-technicians-functions comments deleted

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -1,19 +1,15 @@
 const technicians = require ('../data/technicians-data.json');
-//GET ALL TECHNICIANS
 const getAllTechnicians = () => {
     return technicians;
 }
-//GET TECHNICIANS BY ID
 const getTechnicianById = (id) => {
     const technician = technicians.find(technician => technician.id.toString() === id);
     return technician;
 }
-//GET TECHNICIANS BY ATTRIBUTE
 const getTechniciansByAttribute = (attr, val) => {
     const technician = technicians.filter(tech => tech[attr] === val);
     return technician;
 }
-//DELETE TECHNICIANS
 const deleteTechnicianById = (id) => {
     const technician = technicians.filter(technician => technician.id.toString() !== id);
     return technician;
