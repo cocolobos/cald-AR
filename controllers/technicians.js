cda95f38a6b0f37b30490cce1552bdaf97e502e1
Nicolas Lobos <nicolasmlobos@gmail.com>
2020-11-24 19:39:53 -0300

    CAL-2-technicians-functions completed

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -1,36 +1,27 @@
-const express = require ('express');
-const app = express();
 const technicians = require ('../data/technicians-data.json');
-
 //GET ALL TECHNICIANS
-app.get('/technicians', (req, res) => res.json(technicians))
-
+const getAllTechnicians = () => {
+    return technicians;
+}
 //GET TECHNICIANS BY ID
-app.get('/technicians/:id', (req, res) => {
-    const found = technicians.some(tech => tech.id === parseInt(req.params.id));
-    if(found){
-        res.json(technicians.filter(tech => tech.id === parseInt(req.params.id)));
-    }else{
-        res.status(400).json({msg: "No technician was found"})
-    }
-})
-//DELETE TECHNICIANS
-app.delete('/technicians/:id', (req,res) => {
-    const found = technicians.some(tech => tech.id === parseInt(req.params.id));
-    if(found){
-        res.json(technicians.filter(tech => tech.id !== parseInt(req.params.id)));
-    }else{
-        res.status(400).json({msg:"Technician not found"})
-    }
-})
+const getTechnicianById = (id) => {
+    const technician = technicians.find(technician => technician.id.toString() === id);
+    return technician;
+}
 //GET TECHNICIANS BY ATTRIBUTE
-app.get('/technicians/:attribute/:value', (req, res) => {
-    const parseValue = isNaN(parseInt(req.params.value))? req.params.value : parseInt(req.params.value);
-    const found = technicians.some(tech => tech[req.params.attribute] === parseValue);
-    if(found){
-        res.json(technicians.filter(tech => tech[req.params.attribute] === parseValue));
-    }else{
-        res.status(400).json({msg:"Technician not found"})
-    }
-})
-module.exports = app;
+const getTechniciansByAttribute = (attr, val) => {
+    const technician = technicians.filter(tech => tech[attr] === val);
+    return technician;
+}
+//DELETE TECHNICIANS
+const deleteTechnicianById = (id) => {
+    const technician = technicians.filter(technician => technician.id.toString() !== id);
+    return technician;
+}
+module.exports = {
+    getAllTechnicians,
+    getTechnicianById,
+    getTechniciansByAttribute,
+    deleteTechnicianById
+};
+
--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require ('express');
 const app = express();
-const PORT = process.env.PORT || 5000
-app.use('/', require ('./controllers/technicians'));
+const PORT = process.env.PORT || 5000;
+const techniciansController = require ('./controllers/technicians');
+app.get('/getAllTechnicians', (req, res) => {
+    const technicians = techniciansController.getAllTechnicians();
+    res.json(technicians);
+});
+app.get('/getTechnicianById/:id',(req,res) => {
+    const id = req.params.id;
+    const technicians = techniciansController.getTechnicianById(req.params.id);
+    if(technicians){
+        res.json(technicians);
+    }else{
+        res.send('No technician found with the Id: ' + id);
+    }
+});
+app.get('/getTechnicianByAttribute/:attribute/:value',(req,res) => {
+    const technicians = techniciansController.getTechniciansByAttribute(req.params.attribute, req.params.value);
+    if(technicians){
+        res.json(technicians);
+    }else{
+        res.send('Attribute not found');
+    }
+});
+app.get('/deleteTechnicianById/:id', (req,res) => {
+    const id = req.params.id;
+    const technicians = techniciansController.deleteTechnicianById(req.params.id);
+    res.json(technicians);
+});
 app.listen (PORT, () => console.log("hi"));
