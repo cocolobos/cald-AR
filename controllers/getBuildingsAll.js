da54bd5f4058e18f5f5defb6bbdf1af9f69ffb54
Alejandro Lupo <alejandro.lupo@radiumrocket.com>
2020-11-26 16:41:45 -0300

    delete files

--- controllers/getBuildingsAll.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const router = express.Router();
-const buldings = require('../data/buldings-data.json');
-
-router.get('/', (req,res) => {res.json(buldings);});
-
-module.exports = router;
