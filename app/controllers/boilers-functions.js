dfc279d50b933153664ffc00d12eb9287d956693
Gr4L22 <umarinzalda@gmail.com>
2020-11-28 11:51:46 -0300

    CAL-21 First commit

---
+++ app/controllers/boilers-functions.js
@@ -0,0 +1,33 @@
+const boilers = require("../../data/boilers-data.json");
+
+const getAllBoilers = () => {
+  return boilers;
+};
+
+const getBoilersById = (id) => {
+  const boilerId = boilers.filter((boilers) => {
+    return boilers.id === (id);
+  });
+  return boilerId;
+};
+
+const getBoilerType = (boilerType) => {
+  const boilerTypeFind = boilers.filter((boilers) => {
+    return boilers.boilerType === (boilerType);
+  });
+  return boilerTypeFind;
+};
+
+const deleteBoilerById = (id) => {
+  const boilerId = boilers.filter((boilers) => {
+    return boilers.id !== (id);
+  });
+  return boilerId;
+};
+
+module.exports = {
+  getAllBoilers,
+  getBoilersById,
+  getBoilerType,
+  deleteBoilerById
+};
---
+++ app/controllers/customers.js
@@ -0,0 +1,56 @@
+const customers = require("../../data/customers-data.json");
+
+const getAllCustomers = () => {
+  return customers;
+};
+const getCustomerById = (id) => {
+  const customerId = customers.find((customer) => {
+    return customer.id === (id);
+  });
+  return customerId;
+};
+
+const getCustomerType = (customerType) => {
+  const customerTypeFind = customers.filter((customer) => {
+    return customer.customerType === (customerType);
+  });
+  return customerTypeFind;
+};
+
+const getCustomerByEmail = (email) => {
+  const customerEmail = customers.find((customer) => {
+    return customer.email === (email);
+  });
+  return customerEmail;
+};
+
+const getCustomersByBuildings = (buildings) => {
+  const customerByBuildings = customers.filter((customer) => {
+    return customer.buildings === (buildings);
+  });
+  return customerByBuildings;
+};
+
+const getCustomerByAddress = (fiscal_address) => {
+  const customerByAdress = customers.find((customer) => {
+    return customer.fiscal_address === (fiscal_address);
+  });
+  return customerByAdress;
+};
+
+const deleteCustomersById = (id) => {
+  const customerId = customers.find((customer) => {
+    return customer.id === (id);
+  });
+  return customerId;
+};
+
+module.exports = {
+  getAllCustomers,
+  getCustomerById,
+  getCustomerType,
+  getCustomerByEmail,
+  getCustomersByBuildings,
+  getCustomerByAddress,
+  deleteCustomersById
+};
