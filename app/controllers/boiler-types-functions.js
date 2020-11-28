f9d8dbd7d2712892cbd953569f7fb9bd212ca987
Gr4L22 <umarinzalda@gmail.com>
2020-11-28 11:51:46 -0300

    CAL-21 First commit

---
+++ app/controllers/boiler-types-functions.js
@@ -0,0 +1,33 @@
+const boilerTypes = require("../../data/boiler-types.json");
+
+const getAllBoilerTypes = () => {
+  return boilerTypes;
+};
+
+const getTypesById = (id) => {
+  const typesId = boilerTypes.filter((boilerTypes) => {
+    return boilerTypes.id === (id);
+  });
+  return typesId;
+};
+
+const getStock = (boilerStock) => {
+  const boilerStockFind = boilerTypes.filter((boilerTypes) => {
+    return boilerTypes.boilerStock === (boilerStock);
+  });
+  return boilerStockFind;
+};
+
+const deleteTypeById = (id) => {
+  const typesId = boilerTypes.filter((boilerTypes) => {
+    return boilerTypes.id !== (id);
+  });
+  return typesId;
+};
+
+module.exports = {
+    getAllBoilerTypes,
+    getTypesById,
+    getStock,
+    deleteTypeById
+};
