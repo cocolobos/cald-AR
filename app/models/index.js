2bbd9c380c1542e218c7aa83d59eb41a14a6967f
Gr4L22 <umarinzalda@gmail.com>
2020-11-30 17:59:52 -0300

    Server not working, al functions done

--- app/models/index.js
+++ app/models/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const db = {};
 db.mongoose = mongoose;
 db.url = process.env.DB_URL;
+
 db.appointments = require("./appointments.js")(mongoose);
 db.technicians = require("./technicians.js")(mongoose);
 db.boilersTypes=require("./boilers-types")(mongoose);
