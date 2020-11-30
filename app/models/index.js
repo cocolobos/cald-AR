310dba5dd4ab0a4a13eac40849c3abb264c8dc9c
Gr4L22 <umarinzalda@gmail.com>
2020-11-30 20:23:58 -0300

    CAL-21 Add postman collection

--- app/models/index.js
+++ app/models/index.js
@@ -8,5 +8,6 @@ db.technicians = require("./technicians.js")(mongoose);
 db.boilersTypes=require("./boilers-types")(mongoose);
 db.customers = require("./customers.js")(mongoose);
 db.appointments = require("./buildings")(mongoose);
+db.buildings = require("./buildings")(mongoose);
 
 module.exports = db;
