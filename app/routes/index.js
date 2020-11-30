09b231128e7dfcd78336e7013bb673471c156775
Gr4L22 <umarinzalda@gmail.com>
2020-11-30 18:53:05 -0300

    Minor fix in routers files

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,12 @@ const appointmentsRouter = require("./appointments");
 const techniciansRouter = require("./technicians");
 const boilersTypeRouter = require("./boilers-types");
 const customersRouter = require("./customers");
-
+const buildingsRouter = require('./buildings');
 var router = require("express").Router();
 
 router.use("/appointments", appointmentsRouter);
 router.use("/technicians", techniciansRouter);
 router.use("/boilersTypes", boilersTypeRouter);
 router.use("/customers", customersRouter);
-
+router.use('/buildings', buildingsRouter);
 module.exports = router;
