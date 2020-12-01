0285492eaea69449292224794c48ba694cbba76c
Alejandro Lupo <alejandro.lupo@radiumrocket.com>
2020-12-01 18:03:58 -0300

    CAL-18: update update method and postman collection

--- app/routes/boilers.js
+++ app/routes/boilers.js
@@ -4,7 +4,7 @@ var router = require("express").Router();
 router.get("/", boilers.findAll);
 router.post("/", boilers.create);
 router.get("/:id", boilers.findOne);
-router.post("/:id", boilers.update);
+router.put("/:id", boilers.update);
 router.delete("/:id", boilers.delete);
 
 module.exports = router;
