50e5a6fdc9a4ff946d3ad7b2bfdc38c6ca6f3749
Gr4L22 <umarinzalda@gmail.com>
2020-12-04 16:43:38 -0300

    CAL-22 test 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
 };
