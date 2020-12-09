772a4238ddace88f94901a1225aebe8ea7f75144
Alejandro Lupo <alejandro.lupo@radiumrocket.com>
2020-12-09 18:29:03 -0300

    update

--- app/models/customers.js
+++ app/models/customers.js
@@ -6,6 +6,7 @@ module.exports = (mongoose) => {
         id: {
           type: Number,
           required: true,
+          index: true,
           unique: true,
         },
         customerType: {
